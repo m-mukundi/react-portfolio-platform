e658b889826bf0a9a942addacc3b3dff900c19c3
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2026-05-31 21:17:46 +0300

    Update ProjectFormContainer.jsx

--- src/components/ProjectFormContainer.jsx
+++ src/components/ProjectFormContainer.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./ProjectFormContainer.module.css";
+import styles from "../styles/ProjectFormContainer.module.css";
 
 function ProjectFormContainer({ onAddProject }) {
 	const [title, setTitle] = useState("");
